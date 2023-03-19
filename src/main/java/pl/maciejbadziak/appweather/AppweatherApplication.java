36bb285b454d42db21aaa068f218171ddc7922d3
pan-termometr <termomemtr@gmail.com>
2023-03-19 12:36:02 +0100

    initial commit

---
+++ src/main/java/pl/maciejbadziak/appweather/AppweatherApplication.java
@@ -0,0 +1,13 @@
+package pl.maciejbadziak.appweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppweatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppweatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/maciejbadziak/appweather/AppweatherApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.maciejbadziak.appweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppweatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
